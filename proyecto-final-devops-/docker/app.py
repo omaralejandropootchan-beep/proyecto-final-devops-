549c3a0305711e5192e826b3e6c61facfb7ce3d2
EC2 Default User <ec2-user@ip-172-31-58-21.ec2.internal>
2026-05-12 07:55:28 +0000

    feat: proyecto final completado - docker y ci/cd funcionales

--- proyecto-final-devops-/docker/app.py
+++ proyecto-final-devops-/docker/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Proyecto DevOps - Omar Alejandro</h1><p>Aplicación Flask funcionando tras Nginx</p>"
+    return "¡Hola Omar! Aplicacion Flask corriendo en Docker para el proyecto final."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
